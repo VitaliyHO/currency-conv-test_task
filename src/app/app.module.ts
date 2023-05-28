58048ce85ccdd21f6f6892a2aecbf143c8939ae3
Віталій Гоцуляк <shuricks007@gmail.com>
2023-05-28 21:07:59 +0300

    added main markup & logic of top bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ConverterComponent } from './converter/converter.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ConverterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/converter/converter.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.css']
+})
+export class ConverterComponent {
+  // @Input() currencies: {}
+}
